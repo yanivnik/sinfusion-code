8f9f0751a7ef9a1b2f603679158925ff6c8ca5b9
nivha <nivha@users.noreply.github.com>
2022-09-15 12:26:36 +0300

    settings.py; sample to use settings.py

---
+++ settings.default.py
@@ -0,0 +1,3 @@
+datasets_dir = '/home/yanivni/data/remote_projects/single-image-diffusion'
+results_dir = '/home/yanivni/data/projects_data/single_image_diffusion/'
+# models_dir = '/home/user/data/projects_data/dataset_extraction/models'
